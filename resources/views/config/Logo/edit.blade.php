1f1f515011792a957e168b9f5e88dbf6160fc931
daudangtung1 <dangtung.dau.bka@gmail.com>
2020-08-25 14:53:57 +0700

    logo 1.0.1

--- resources/views/config/Logo/edit.blade.php
+++ resources/views/config/Logo/edit.blade.php
@@ -2,14 +2,24 @@
 @section('title', 'Edit')
 @section('content')
     <div class="container">
-        <form method="post" action="{{route('logoUpdate',$edit)}}" enctype="multipart/form-data">
-            {{csrf_field()}}
-            @method('POST')
-            <div class="form-group">
-                <label>Image</label>
-                <input type="file" class="form-control" name="image" value="{{ $edit->image }}">
+        <div class="row">
+            <div class="col-md-8">
+                <form method="post" action="{{route('logoUpdate',$edit)}}" enctype="multipart/form-data">
+                    {{csrf_field()}}
+                    @method('POST')
+                    <div class="form-group">
+                        <div class="input-group">
+                <span class="input-group-btn">
+                <input type="file" class="form-control" name="image" value="{{ $edit->image }}" id="imgInp">
+                    </span>
+                        </div>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </form>
             </div>
-            <button type="submit" class="btn btn-primary">Submit</button>
-        </form>
+            <div class="col-md-4">
+                <img id='img-upload'/>
+            </div>
+        </div>
     </div>
 @endsection
